d53232b4637cf822093313f692f9e82cc830a87b
Bhakti Muhtadin Rizaldi <50583241+bhaktirizaldi@users.noreply.github.com>
2020-12-20 20:13:15 +0900

    Update 2. 1. simple_linear_regression.py

--- 2. 1. simple_linear_regression.py
+++ 2. 1. simple_linear_regression.py
@@ -12,18 +12,18 @@ y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Training the Simple Linear Regression model on the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+regressor.fit(X_train, y_train)                                  # use the object in the train dataset to train 
 
 # Predicting the Test set results
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(X_test)                               # create the prediction by using independent data set
 
 # Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
+plt.scatter(X_train, y_train, color = 'red')                      
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
 plt.title('Salary vs Experience (Training set)')
 plt.xlabel('Years of Experience')
